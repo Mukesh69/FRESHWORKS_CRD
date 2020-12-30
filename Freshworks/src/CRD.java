fca5a2652443895db7eb5fc27b135c3d36bc7cef
Mukesh <43651552+Mukesh69@users.noreply.github.com>
2020-12-30 20:14:39 +0530

    Commit (Create)

--- Freshworks/src/CRD.java
+++ Freshworks/src/CRD.java
@@ -8,7 +8,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CRD {
-	
 	//To fix the file storage path
 	private File dir;
     Scanner scan;
@@ -22,9 +21,41 @@ public class CRD {
     }
     
     //Create a new key value pair file
+    public void create(String path){
+        FileWriter file;
+        String key,nv="";
+        String[] name_value;
+        JSONObject obj = new JSONObject();
+        System.out.print("ENTER FILE NAME : ");key = scan.next();
+        System.out.println("ENTER KEY AND VALUE (KEY,VALUE).TYPE 'END' TO TERMINATE: ");
+        while(true){
+            nv = scan.next();
+            if(nv.equalsIgnoreCase("end")) break;
+            else {
+                name_value = nv.split(",", 2);
+                try {
+                    obj.put(name_value[0], name_value[1]);
+                    System.out.println(name_value[0] + "-" + name_value[1]);
+                } catch(ArrayIndexOutOfBoundsException e){
+                  break;
+                } catch (JSONException e) {
+                    System.out.print("UNABLE TO LOAD JSON");
+                    e.printStackTrace();
+                }
+            }
+        }
+        try{
+            file = new FileWriter(path+"/"+key+".txt");
+            file.write(obj.toString());
+            System.out.println("JSON OBJECT : \n"+ obj);
+            file.flush();
+            file.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
     
-    
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		 String def_path = "C:/Data Store";       
 	        String path = "";
